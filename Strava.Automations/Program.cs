b63ed09d2f907c9830c5f55544901a0754f2cf5b
jtauanpm <jtauan10@gmail.com>
2025-11-26 21:41:34 -0300

    no access token

---
+++ Strava.Automations/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Strava.Automations.Services;
+
+var accessToken = "--";
+
+await StravaActivityService.AdicionarTenisParaAtividadesOutubro2025(accessToken);
+
+
+
