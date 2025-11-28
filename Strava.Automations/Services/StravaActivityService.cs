c8d4fa2f2278f719a0a4bbf7655fb08f3523f4e5
jtauanpm <jtauan10@gmail.com>
2025-11-28 19:40:44 -0300

    strava activity service with my interest automation

---
+++ Strava.Automations/Services/StravaActivityService.cs
@@ -0,0 +1,24 @@
+using Strava.Automations.Entities;
+using Strava.Automations.ExternalServices;
+
+namespace Strava.Automations.Services;
+
+public static class StravaActivityService
+{
+    private const string IdTenisOlympikusMarte = "g27381848";
+    
+    public static async Task AdicionarTenisParaAtividadesOutubro2025(string accessToken)
+    {
+        var outubro2025 = new DateTime(2025, 10, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        var atividades = await StravaApi.ObterAtividades(accessToken, outubro2025);
+        var atividadesDescalco = atividades.Where(a => a.IdTenis == null).ToList();
+
+        Console.WriteLine($"Atividades descalço: {atividadesDescalco.Count}");
+
+        foreach (var a in atividadesDescalco)
+        {
+            await StravaApi.AtualizarTenisParaAtividade(a.Id, IdTenisOlympikusMarte, accessToken);
+        }
+    }
+}
